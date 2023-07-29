eb6c1eb07278342fa695e6008ec08c1323a0824d
HeyAdin <adinahmad003@gmail.com>
2023-07-29 14:49:15 +0530

    made each row display flex and added flex property all the grid became flex item and assigned flex property on them earlier grid was set with a size now it is occupying space using flex:1 property

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const gridContainer = document.querySelector('.grid-container');
 
 for(let i =0; i<16 ; i++){
     const gridWrap= document.createElement('div');
+    gridWrap.setAttribute('style', 'display:flex;flex-direction:column;flex:1;')
     for(let j =0; j<16;j++){
         const div = document.createElement('div');
-        div.setAttribute('style', 'height:30px; width: 30px; border: 1px solid black;');
+        div.setAttribute('style', 'border: 1px solid black; flex:1; ');
         gridWrap.appendChild(div);
     }
     gridContainer.appendChild(gridWrap);
