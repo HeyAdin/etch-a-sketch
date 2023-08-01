7c51d6ad0bfa21f6b6ff1043c5154f1233e37bd1
HeyAdin <adinahmad003@gmail.com>
2023-08-01 16:05:07 +0530

    Added Event listener for giving pen look restricted usert to input more than 200 grids

--- script.js
+++ script.js
@@ -1,53 +1,75 @@
 const gridContainer = document.querySelector('.grid-container');
-const mainContainer=document.querySelector('.main-container');
+const mainContainer = document.querySelector('.main-container');
 const button = document.querySelector('button');
-const sizeText=document.createElement('p');
+const sizeText = document.createElement('p');
 
 
-let size=16;
+let size = 16;
 
-sizeText.textContent="Current Grid Size = "+size+" x "+size;
-sizeText.setAttribute('id','sizeText');
-mainContainer.insertBefore(sizeText,button);
+sizeText.textContent = "Current Grid Size = " + size + " x " + size;
+sizeText.setAttribute('id', 'sizeText');
+mainContainer.insertBefore(sizeText, button);
 
-    for(let i =0; i<size ; i++){
-        const gridWrap= document.createElement('div');
-        gridWrap.classList.add('wrapper');
-        gridWrap.setAttribute('style', 'display:flex;flex-direction:column;flex:1;')
-        for(let j =0; j<size;j++){
-            const div = document.createElement('div');
-            div.classList.add('grid');
-            div.setAttribute('style', 'border: 1px solid gray; flex:1; ');
-            gridWrap.appendChild(div);
-        }
-        gridContainer.appendChild(gridWrap);
+for (let i = 0; i < size; i++) {
+    const gridWrap = document.createElement('div');
+    gridWrap.classList.add('wrapper');
+    gridWrap.setAttribute('style', 'display:flex;flex-direction:column;flex:1;')
+    for (let j = 0; j < size; j++) {
+        const div = document.createElement('div');
+        div.classList.add('grid');
+        div.setAttribute('style', 'flex:1; ');
+        gridWrap.appendChild(div);
     }
+    gridContainer.appendChild(gridWrap);
+}
 
-    
+const allgrid = document.querySelectorAll('.grid');
 
 
-button.addEventListener('click',()=>{
+allgrid.forEach((grid)=>{
+    grid.addEventListener('mouseenter',()=>{
+        grid.classList.add('color');
+    });
+});
+
+
+button.addEventListener('click', () => {
     const allWrapper = document.querySelectorAll('.wrapper');
-    const allgrid = document.querySelectorAll('.grid');
-    allWrapper.forEach( (wrapper) => {
+    let allgrid = document.querySelectorAll('.grid');
+
+    allWrapper.forEach((wrapper) => {
         wrapper.remove();
     });
-    allgrid.forEach( (grid) => {
+
+    allgrid.forEach((grid) => {
         grid.remove();
     });
-    size=prompt("Enter Number Between 1 to 100");
-    sizeText.textContent="Current Grid Size = "+size+" x "+size;
 
-    for(let i =0; i<size ; i++){
-        const gridWrap= document.createElement('div');
+    size = prompt("Enter Number Between 1 to 100");
+    if(size>0 && size<=200){
+        sizeText.textContent = "Current Grid Size = " + size + " x " + size;
+
+    for (let i = 0; i < size; i++) {
+        const gridWrap = document.createElement('div');
         gridWrap.classList.add('wrapper');
         gridWrap.setAttribute('style', 'display:flex;flex-direction:column;flex:1;')
-        for(let j =0; j<size;j++){
+        for (let j = 0; j < size; j++) {
             const div = document.createElement('div');
             div.classList.add('grid');
-            div.setAttribute('style', 'border: 1px solid gray; flex:1; ');
+            div.setAttribute('style', ' flex:1; ');
             gridWrap.appendChild(div);
         }
         gridContainer.appendChild(gridWrap);
     }
+    allgrid = document.querySelectorAll('.grid');
+    allgrid.forEach((grid)=>{
+    grid.addEventListener('mouseenter',()=>{
+        grid.classList.add('color');
+    });
+});
+    }
+    else{
+        alert("Try Again and Enter size in between give value");
+    }
+    
 });
